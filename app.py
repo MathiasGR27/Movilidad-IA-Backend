74d7b3414f8c95e2f96eefdd4507dd15a5255174
MathiasGR27 <megualpa@espe.edu.ec>
2026-07-18 20:08:49 -0500

    backend preparado para deploy

--- app.py
+++ app.py
@@ -11,37 +11,92 @@ from routes.geojson_routes import geojson_bp
 from routes.chat_routes import chat_bp
 from routes.historial_routes import historial_bp
 from routes.user_routes import user_bp
-from routes.conversacion_routes import (conversacion_bp)
+from routes.conversacion_routes import conversacion_bp
+
 
 load_dotenv()
 
-app = Flask(__name__)
-CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app = Flask(__name__)
+
+
+CORS(
+    app,
+    resources={
+        r"/api/*": {
+            "origins": "*"
+        }
+    }
+)
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URL"
+)
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+
 db.init_app(app)
 
-app.register_blueprint(auth_bp, url_prefix="/api/auth")
-app.register_blueprint(geojson_bp, url_prefix="/api")
-app.register_blueprint(chat_bp, url_prefix="/api")
-app.register_blueprint(historial_bp,url_prefix="/api")
-app.register_blueprint(user_bp,url_prefix="/api")
-app.register_blueprint(conversacion_bp,url_prefix="/api")
+
+
+# ==========================
+# RUTAS
+# ==========================
+
+app.register_blueprint(
+    auth_bp,
+    url_prefix="/api/auth"
+)
+
+app.register_blueprint(
+    geojson_bp,
+    url_prefix="/api"
+)
+
+app.register_blueprint(
+    chat_bp,
+    url_prefix="/api"
+)
+
+app.register_blueprint(
+    historial_bp,
+    url_prefix="/api"
+)
+
+app.register_blueprint(
+    user_bp,
+    url_prefix="/api"
+)
+
+app.register_blueprint(
+    conversacion_bp,
+    url_prefix="/api"
+)
+
+
 
 @app.route("/")
 def home():
 
     return {
-        "mensaje": "Backend movilidad IA funcionando"
+        "mensaje":
+        "Backend movilidad IA funcionando"
     }
 
+
+
+# Crear tablas
+
 with app.app_context():
+
     db.create_all()
 
+
+
 if __name__ == "__main__":
+
     app.run(
         host="0.0.0.0",
         port=5000
--- config.py
+++ config.py
@@ -3,5 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
-    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+    GEMINI_API_KEY = os.getenv(
+        "GEMINI_API_KEY"
+    )
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
